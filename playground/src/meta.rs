6ffe830892389c90ed9d8b6ec09132d912bff3bf
Ryan Lopopolo <rjl@hyperbo.la>
2019-09-15 21:51:46 -0700

    yarn lint

--- playground/src/meta.rs
+++ playground/src/meta.rs
@@ -5,11 +5,11 @@ use artichoke_core::value::Value;
 pub fn build_info(interp: &Artichoke) -> String {
     let description = interp
         .eval("RUBY_DESCRIPTION")
-        .and_then(|desc| desc.try_into::<&str>())
+        .and_then(Value::try_into::<&str>)
         .unwrap_or_default();
     let compiler = interp
         .eval("ARTICHOKE_COMPILER_VERSION")
-        .and_then(|comp| comp.try_into::<&str>())
+        .and_then(Value::try_into::<&str>)
         .unwrap_or_default();
     format!("{}\n[{}]", description, compiler)
 }
