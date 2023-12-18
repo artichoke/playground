52dbe202973a4d1ce1c504debe6143d284b6d7bc
Ryan Lopopolo <rjl@hyperbo.la>
2023-12-18 11:10:16 -0800

    npm run fmt

--- src/main.ts
+++ src/main.ts
@@ -22,33 +22,33 @@ import example from "./examples/forwardable_regexp_io.rb?raw";
 //
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
 (self as any).MonacoEnvironment = {
-    getWorker(_, label) {
-        switch (label) {
-            case 'json':
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-                return new jsonWorker();
-            case 'css':
-            case 'scss':
-            case 'less':
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-                return new cssWorker();
-            case 'html':
-            case 'handlebars':
-            case 'razor':
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-                return new htmlWorker();
-            case 'typescript':
-            case 'javascript':
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-                return new tsWorker();
-            case 'yaml':
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-                return new yamlWorker();
-            default:
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-                return new editorWorker()
-        }
+  getWorker(_, label) {
+    switch (label) {
+      case "json":
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+        return new jsonWorker();
+      case "css":
+      case "scss":
+      case "less":
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+        return new cssWorker();
+      case "html":
+      case "handlebars":
+      case "razor":
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+        return new htmlWorker();
+      case "typescript":
+      case "javascript":
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+        return new tsWorker();
+      case "yaml":
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+        return new yamlWorker();
+      default:
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+        return new editorWorker();
     }
+  },
 };
 
 // The playground serializes the content of the code editor into the URL
