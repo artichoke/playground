098f0e453426b6c825434a22ccd84e9c55ae0681
Ryan Lopopolo <rjl@hyperbo.la>
2025-03-03 21:22:06 -0800

    fix unused var warning by inline type brands

--- src/wasm/playground.d.ts
+++ src/wasm/playground.d.ts
@@ -1,15 +1,12 @@
 declare function Module(): Module.Thenable;
 
-declare const ARTICHOKE_TYPE: unique symbol;
-declare const STRING_POINTER_TYPE: unique symbol;
-
 declare namespace Module {
   export class Thenable {
     public then(thunk: (wasm: Ffi) => void): void;
   }
 
-  export type Artichoke = number & { _opaque: typeof ARTICHOKE_TYPE };
-  export type StringPointer = number & { _opaque: typeof STRING_POINTER_TYPE };
+  export type Artichoke = number & { _opaque: unique symbol };
+  export type StringPointer = number & { _opaque: unique symbol };
 
   export class Ffi {
     public _artichoke_web_repl_init(): Artichoke;
@@ -18,17 +15,21 @@ declare namespace Module {
       state: Artichoke,
       ptr: StringPointer,
     ): number;
+
     public _artichoke_string_getch(
       state: Artichoke,
       ptr: StringPointer,
       index: number,
     ): number;
+
     public _artichoke_string_new(state: Artichoke): StringPointer;
-    _artichoke_string_putch(
+
+    public _artichoke_string_putch(
       state: Artichoke,
       ptr: StringPointer,
       byte: number,
     ): void;
+
     public _artichoke_string_free(state: Artichoke, ptr: StringPointer): void;
 
     public _artichoke_eval(
